ea15498727c263c3e3c8b2dbcc104beabf255b0d
Fazila <fazilaali2001@gmail.com>
2026-03-30 05:46:52 +0500

    Updated something

--- main/java/config/Configuration.java
+++ main/java/config/Configuration.java
@@ -6,11 +6,11 @@ public class Configuration {
     public static final boolean HEADLESS = false;
     public static final int TIMEOUT = 30000;
     public static final int TIMEOUTLogin = 300000;
-    public static final String BASE_URL = "http://69.30.247.3:8089/";
-    public static final String IP = "69.30.247.3";
-    public static final String PORT = "8088";
-    public static final String USERNAME = "administrator";
-    public static final String PASSWORD = "Admin123!";
+    public static final String BASE_URL = "Confidential";
+    public static final String IP = "Confidential";
+    public static final String PORT = "Confidential";
+    public static final String USERNAME = "Confidential";
+    public static final String PASSWORD = "Confidential";
 
 }
 
